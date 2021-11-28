7912ea9619ce4ec02c9110ee7192f0e9fec32224
0xburden <0xburden@protonmail.com>
2021-11-27 16:06:55 -0800

    more fixes for song player

--- src/components/SongPlayer/SongPlayer.tsx
+++ src/components/SongPlayer/SongPlayer.tsx
@@ -15,6 +15,7 @@ export default function SongPlayer({ first, songs, gifs }: Props) {
   const [currentGif, setCurrentGif] = React.useState<string | null>(null);
   const [currentTime, setCurrentTime] = React.useState<number>(0);
   const [isPlaying, setPlaying] = React.useState<boolean>(false);
+  const [isLoading, setLoading] = React.useState<boolean>(false);
   const audioRef = React.useRef<HTMLAudioElement | null>(null);
 
   const getRandomIndex = (arr) => arr[Math.floor(Math.random() * arr.length)];
@@ -24,26 +25,37 @@ export default function SongPlayer({ first, songs, gifs }: Props) {
     setCurrentSong(first);
   }, []);
 
-  const lastTrack = React.useCallback(() => {
+  const lastTrack = React.useCallback(async () => {
     if (audioRef.current === null) {
       return;
     }
 
     setCurrentSong(lastSong);
-    audioRef.current.load();
+    await audioRef.current.load();
     audioRef.current.play();
   }, [audioRef, lastSong]);
 
-  const nextTrack = React.useCallback(() => {
+  const nextTrack = React.useCallback(async () => {
     if (audioRef.current === null) {
       return;
     }
 
+    let nextSong = getRandomIndex(songs);
+    if (nextSong === currentSong) {
+      nextSong = getRandomIndex(songs);
+    }
     setLastSong(currentSong);
-    setCurrentSong(getRandomIndex(songs));
-    audioRef.current.load();
+    setCurrentSong(nextSong);
+    await audioRef.current.load();
     audioRef.current.play();
   }, [audioRef, currentSong]);
+
+  const currentProgress = React.useMemo(() => {
+    if (audioRef.current === null) {
+      return 0;
+    }
+    return parseInt((currentTime / audioRef.current.duration) * 100, 10);
+  }, [currentTime]);
 
   React.useEffect(() => {
     const interval = setInterval(() => setCurrentGif(getRandomIndex(gifs)), 10000);
@@ -52,30 +64,53 @@ export default function SongPlayer({ first, songs, gifs }: Props) {
   }, [currentGif]);
 
   React.useEffect(() => {
-    if (audioRef.current !== null) {
-      audioRef.current.addEventListener('ended', () => {
-        let nextSong = getRandomIndex(songs);
-        if (nextSong === first) {
-          nextSong = getRandomIndex(songs);
-        }
-        setCurrentSong(nextSong);
-        audioRef.current.load();
-        audioRef.current.play();
-      });
-
-      audioRef.current.addEventListener('stalled', () => {
-        setCurrentSong(getRandomIndex(songs));
-        audioRef.current.load();
-        audioRef.current.play();
-      });
-
-      audioRef.current.addEventListener('pause', () => setPlaying(false));
-      audioRef.current.addEventListener('play', () => setPlaying(true));
+    if (audioRef.current === null) {
+      return;
     }
+
+    const endedListener = async () => {
+      let nextSong = getRandomIndex(songs);
+      if (nextSong === first) {
+        nextSong = getRandomIndex(songs);
+      }
+      setCurrentSong(nextSong);
+      await audioRef.current.load();
+      audioRef.current.play();
+    };
+
+    const stalledListener = async () => {
+      setCurrentSong(getRandomIndex(songs));
+      await audioRef.current.load();
+      audioRef.current.play();
+    };
+
+    const pauseListener = () => setPlaying(false);
+
+    const playListener = () => setPlaying(true);
+
+    const loadStartListener = () => setLoading(true);
+
+    const loadedDataListener = () => setLoading(false);
+
+    audioRef.current.addEventListener('ended', endedListener);
+    audioRef.current.addEventListener('stalled', stalledListener);
+    audioRef.current.addEventListener('pause', pauseListener);
+    audioRef.current.addEventListener('play', playListener);
+    audioRef.current.addEventListener('loadstart', loadStartListener);
+    audioRef.current.addEventListener('loadeddata', loadedDataListener);
+
+    return () => {
+      audioRef.current.removeEventListener('ended', endedListener);
+      audioRef.current.removeEventListener('stalled', stalledListener);
+      audioRef.current.removeEventListener('pause', pauseListener);
+      audioRef.current.removeEventListener('play', playListener);
+      audioRef.current.removeEventListener('loadstart', loadStartListener);
+      audioRef.current.removeEventListener('loadeddata', loadedDataListener);
+    };
   }, [audioRef, setCurrentSong, getRandomIndex, songs]);
 
   React.useEffect(() => {
-    if (!isPlaying) {
+    if (!isPlaying || audioRef.current === null) {
       return;
     }
 
@@ -92,7 +127,9 @@ export default function SongPlayer({ first, songs, gifs }: Props) {
     <div className={styles['player']}>
       <div className={styles['player--img-container']}>
         {currentGif !== null ? (
-          <img src={`/assets/gifs/${currentGif}`} alt="" className={styles['player--img']} />
+          <div className={styles['player--spinner']}>
+            <img src={`/assets/gifs/${currentGif}`} alt="" className={styles['player--img']} />
+          </div>
         ) : null}
         <img src="/assets/waifusanonymous.png" alt="" className={styles['player--logo']} />
       </div>
@@ -111,12 +148,12 @@ export default function SongPlayer({ first, songs, gifs }: Props) {
               role="progressbar"
               aria-valuemin={0}
               aria-valuemax={100}
-              aria-valuenow={currentTime}
+              aria-valuenow={currentProgress}
               style={{ width: currentTime }}
             />
           </div>
           <div className={styles['player--controls']}>
-            <button type="button" disabled={!lastSong} onClick={() => lastTrack()}>
+            <button type="button" disabled={!lastSong || isLoading} onClick={() => lastTrack()}>
               <FontAwesomeIcon icon={faBackward} />
             </button>
             <button
@@ -124,10 +161,11 @@ export default function SongPlayer({ first, songs, gifs }: Props) {
               onClick={() =>
                 audioRef.current.paused ? audioRef.current.play() : audioRef.current.pause()
               }
+              disabled={isLoading}
             >
               {isPlaying ? <FontAwesomeIcon icon={faPause} /> : <FontAwesomeIcon icon={faPlay} />}
             </button>
-            <button type="button" onClick={() => nextTrack()}>
+            <button type="button" onClick={() => nextTrack()} disabled={isLoading}>
               <FontAwesomeIcon icon={faForward} />
             </button>
           </div>
